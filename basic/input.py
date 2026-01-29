9231eec2cdaf5ab3f8e0f19dff04b4f319ccfb62
DhairyaSinghHariom <sdhairya192@gmail.com>
2026-01-29 12:06:31 +0530

    Update input.py

--- basic/input.py
+++ basic/input.py
@@ -7,10 +7,29 @@ print("Hello, " + name + "! You are " + age + " years old.")
 usertext = input("What is your name? ")
 print("Hello", usertext)
 
-# sum two number
+# How to add 2 numbers entered on Python?
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 sum = float(num1) + float(num2)
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# How to find the Average of 2 Entered Numbers on Python?
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+average =(int(num1) + int(num2))
+
+print('average:{0} '.format(average))
+
+
+
+# How to calculate the Entered Visa and Final Grade Average on Python?
+
+visa = float(input("Enter visa grade: "))
+final = float(input("Enter final grade: "))
+
+average = visa * 0.3 + final * 0.7
+
+print("Average:", average)
