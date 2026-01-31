d80b4400f66abc4b29afc142ae1bd64442734a59
DhairyaSinghHariom <sdhairya192@gmail.com>
2026-01-31 14:32:30 +0530

    Implement Bubble Sort and Selection Sort
    Added Bubble Sort and Selection Sort implementations to Insert.py.

--- sort/Insert.py
+++ sort/Insert.py
@@ -26,3 +26,46 @@ for i in range(1, len(arr)):
     arr[j + 1] = key          # insert element
 
 print(arr)
+
+
+
+
+# Bubble Sort 
+"""Compare first two numbers
+
+Swap if the first is bigger
+
+Move to the next pair
+
+Repeat until the list is sorted
+"""
+
+
+arr = [5, 3, 8, 4, 2]
+n = len(arr)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted list:", arr)
+
+
+# Seletion Sort
+
+arr = [5, 3, 8, 4, 2]
+n = len(arr)
+
+for i in range(n):
+    min_index = i
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print("Sorted list:", arr)
+
+
+
