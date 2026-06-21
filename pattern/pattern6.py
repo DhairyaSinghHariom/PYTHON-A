d7cfb8a74e24cf5f2a9bbc0139a1077c18aff53e
Dhairya Singh <sdhairya192@gmail.com>
2026-06-21 17:40:08 +0530

    Add files via upload

---
+++ pattern/pattern6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def pattern1(self, n):
+        for i in range(n, 0, -1):
+            for j in range(1, i + 1):
+                print(j, end=" ")
+            print()
+
+obj = Solution()
+obj.pattern1(5)
+
+# output:
+
+# 12345
+# 1234
+# 123
+# 12    
+# 1
+
+
+class Solution:
+    def pattern1(self, n):
+        for i in range(n, 0, -1):
+            for j in range(1, i + 1):
+                print(j, end=" ")
+            print()
+
+obj = Solution()
+obj.pattern1(2)
+
+# output:
+# 12
+# 1
