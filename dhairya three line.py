f773c5f43c3eb9d8f81d08af0ab64c1039f5712d
Dhairya Singh <sdhairya192@gmail.com>
2026-06-24 08:41:32 +0530

    Delete dhairya three line.py

--- dhairya three line.py
+++
@@ -1,8 +0,0 @@
-print("""1:-low- level progamming is the machine-dependent language (0&1)
-the programms run with out any need of compiler or interpreter
-low- level language can be run very fast""")
-print("""2:-machine language is consist of binary codes
-which direct operated by CPU
-all instruction are written in form of 0&1""")
-print("""3:-assembly language have some symbols and codes that represent the basic operations
-that computer can perform which include adding ,moving,or comparing no""")
